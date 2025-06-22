68d7035d5aaf3162d44eaba3ae8a7d2f66ba1dc5
erskinenyongesawanami <erskineviper@gmail.com>
2025-06-23 01:11:05 +0300

    add changes made

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email
+  };
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    const newId = document.createElement("div");
+    newId.textContent = object.id;
+    document.body.appendChild(newId);
+  })
+  .catch(function (error) {
+    const errorMsg = document.createElement("div");
+    errorMsg.textContent = error.message;
+    document.body.appendChild(errorMsg);
+  });
+}
